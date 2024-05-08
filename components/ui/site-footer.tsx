323e3204b994305a6731d335cbfb4b3fada7838a
Adarsh Raj <raj.adarsh2401@gmail.com>
2024-05-09 01:16:07 +0530

    change footer

--- components/ui/site-footer.tsx
+++ components/ui/site-footer.tsx
@@ -21,7 +21,7 @@ export function SiteFooter() {
             rel="noreferrer"
             className="font-medium underline underline-offset-4"
           >
-            Leetcodeboard
+            Github
           </a>
           .
         </p>
