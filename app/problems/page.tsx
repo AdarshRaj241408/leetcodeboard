f3de2f8ee2bae51f58f1cb7ae50fcd584cf30265
Adarsh Raj <raj.adarsh2401@gmail.com>
2024-05-27 23:37:58 +0530

    form added

--- app/problems/page.tsx
+++ app/problems/page.tsx
@@ -1,10 +1,13 @@
+"use client";
 import { Button, buttonVariants } from "@/components/ui/button";
 import { ModeToggle } from "@/components/ui/mode-toggle";
 import { SiteFooter } from "@/components/ui/site-footer";
 import { SiteHeader } from "@/components/ui/site-header";
 import Link from "next/link";
+import { FormEvent } from "react";
+import { ProfileForm } from "./form";
 
-export default function Home() {
+export default function Problems() {
   return (
     <>
       <SiteHeader />
@@ -12,10 +15,7 @@ export default function Home() {
       <div className="relative flex min-h-screen flex-col justify-center overflow-hidden py-6 sm:py-12">
         <div className="relative px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-500/45 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
           <div className="flex space-x-4">
-            <Link href="/problems">
-              <Button>Hello</Button>
-            </Link>
-            <ModeToggle />
+            <ProfileForm />
           </div>
         </div>
       </div>
