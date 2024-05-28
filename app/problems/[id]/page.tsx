54e25a26928e5ad86087b56399f6563fab2820ee
Adarsh Raj <raj.adarsh2401@gmail.com>
2024-05-29 01:50:55 +0530

    z-index

--- app/problems/[id]/page.tsx
+++ app/problems/[id]/page.tsx
@@ -8,12 +8,13 @@ export default function Page({ params }: { params: { id: string } }) {
     <>
       <SiteHeader />
 
-      <div className="relative flex min-h-screen flex-col justify-center overflow-hidden py-6 sm:py-12">
-        <div className="relative px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-500/45 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
-          <div className="flex space-x-4">Problem ID: {id}</div>
-        </div>
+      <div className="relative flex flex-col justify-center overflow-hidden z-50">
+        <iframe
+          src={`https://excalidraw.com/#${id}`}
+          style={{ width: "100%", height: "92.5vh", border: "none" }}
+          title="Excalidraw"
+        />
       </div>
-
       <SiteFooter />
     </>
   );
