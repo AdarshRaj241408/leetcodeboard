5ab226665a18e6dd68516cbccf4fd3bf4b18b9da
Adarsh Raj <raj.adarsh2401@gmail.com>
2024-06-08 23:31:38 +0530

    welcomescreen error

--- components/ui/excalidrawWrapper.tsx
+++ components/ui/excalidrawWrapper.tsx
@@ -1,25 +1,17 @@
 "use client";
-import {
-  Excalidraw,
-  convertToExcalidrawElements,
-} from "@excalidraw/excalidraw";
+import { Excalidraw, WelcomeScreen } from "@excalidraw/excalidraw";
 
 //import "@excalidraw/excalidraw/index.css";
 
 const ExcalidrawWrapper: React.FC = () => {
-  console.info(
-    convertToExcalidrawElements([
-      {
-        type: "rectangle",
-        id: "rect-1",
-        width: 186.47265625,
-        height: 141.9765625,
-      },
-    ])
-  );
   return (
     <div style={{ height: "92.5vh", width: "100%" }}>
-      <Excalidraw />
+      <Excalidraw>
+        <WelcomeScreen>
+          <WelcomeScreen.Hints.HelpHint />
+          <WelcomeScreen.Hints.HelpHint />
+        </WelcomeScreen>
+      </Excalidraw>
     </div>
   );
 };
