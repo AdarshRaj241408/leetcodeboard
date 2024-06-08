8134c1da96457aa15f579ba8c6d7bfdc9d751369
Adarsh Raj <raj.adarsh2401@gmail.com>
2024-06-09 00:18:46 +0530

    debugging

--- excalidraw.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "@excalidraw/excalidraw" {
-  import * as React from "react";
-
-  export const Excalidraw: React.FC<any>;
-  export function convertToExcalidrawElements(elements: any[]): any[];
-}
